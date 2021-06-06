3987fb67c5716fb8848c3837896d7d27eee6a260
Raiyan <raiyan.sharif.1234@gmail.com>
2021-06-06 21:54:05 +0600

    app changes

---
+++ YamahaRidersClub/Services/AppSettingVariable.swift
@@ -0,0 +1,14 @@
+//
+//  AppSettingVariable.swift
+//  YamahaRidersClub
+//
+//  Created by raiyan sharif on 6/6/21.
+//
+
+import Foundation
+
+class AppSettingVariable{
+    static var isNewRideOn:Bool = false
+    static var viewControllerNewRide:NewRideViewController?
+    static var homeVc:HomeViewController?
+}
